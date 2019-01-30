cb5d3dcb78387f5ea0920ec83f81237fc526f667
LukeDavies12 <luketdavies12@gmail.com>
2019-01-29 20:25:07 -0600

    Add project files.

---
+++ FizzBuzzConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
