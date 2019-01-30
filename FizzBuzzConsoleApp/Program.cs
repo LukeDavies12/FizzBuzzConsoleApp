62311eeb0a55d3496c88631d1b972f2cac8a292b
LukeDavies12 <luketdavies12@gmail.com>
2019-01-29 20:39:59 -0600

    FizzBuzz C#

--- FizzBuzzConsoleApp/Program.cs
+++ FizzBuzzConsoleApp/Program.cs
@@ -6,7 +6,46 @@ namespace FizzBuzzConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Ye!");
+            Console.WriteLine("else if way");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("string way");
+            for (int i = 1; i <= 100; i++)
+            {
+                string s = "";
+                if (i % 3 == 0)
+                {
+                    s += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    s += "Buzz";
+                }
+                if (s.Length == 0)
+                {
+                    s = i.ToString();
+                }
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("FizzBuzz answers c#, Press a key to close..");
+            Console.ReadKey(true);
         }
     }
 }
